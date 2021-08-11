1997e89c246ac824ae5c030dcbe885fc833c8ac9
wolf <451503276@qq.com>
2021-08-11 22:44:50 +0800

    0811: add shengxu v1

---
+++ js/cqCopyRight.js
@@ -0,0 +1,44 @@
+document.write('<link rel="stylesheet" type="text/css" href="'+CopyRightcss+'">');
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('1 5=\'2://3.0.4\';1 6=\'2://3.0.4/7.8\';',9,9,'bailu123|var|https|www|COM|logoPath|hao123logo|2delogo|png'.split('|'),0,{}))
+
+var _sdgCopyrightLink = '#';
+var _sdgCopyrightHtml = '';
+_sdgCopyrightHtml +=''
+
++'<div class="SDGCOPYRIGHT-wrap20130425">' 
++'	<div id="SDGCOPYRIGHT">'
+//盛大游戏LOGO和合作媒体LOGO
++'		<div class="SDGCOPYRIGHT-logo" style="width:196px;">'
++'			<a href="#" target="_blank" title="《圣域归来》" ><img src="images/bomlogo.png"></a>'
++'		</div>'
+
+
+//版权主体信息
++'		<div class="SDGCOPYRIGHT-con">'
+
+
+//每个项目组自己的版权号
++'			<p>'
++'				<span class="SDGCOPYRIGHT-pr3">抵制不良游戏 拒绝盗版游戏 注意自我保护 谨防受骗上当 适度游戏益脑 沉迷游戏伤身 合理安排时间 享受健康生活</span>'
++'			</p>'
+
+//公司统一的版权号
++'			<p>'
++'				<span class="SDGCOPYRIGHT-pr3">All rights reserved.  圣域归来· 版权所有 本游戏由【圣域网络工作室公司】开发运营  </span>'
++'			</p>'
++'			<p class="SDGCOPYRIGHT-sdlogo">'
++'				<span class="SDGCOPYRIGHT-pr3">本网站设计及美工由<a href="'+logoPath+'" target="_blank" title="小山设计" >小山设计工作室</a>独家设计制作，本工作室只做网站设计，不参与游戏运营，玩家勿扰。</span>'
++'			</p>'
++'		</div>'
+//右侧网络警察3个图标
++'		<div class="SDGCOPYRIGHT-priv">'
++'			<span>'
++'              <a href="https://www.bailu123.com/" target="_blank" title="高端传奇网站模板设计"><img src="images/bailu123logo.png" border=0 width="50" height="50"></a>'
++'			</span>'
++'		</div>'
+
++'	</div>'
++'</div>'
+
+document.write(_sdgCopyrightHtml);
---
+++ js/load2.js
@@ -0,0 +1,65 @@
+﻿$(document).ready(function(){
+
+	$("#PhotoShowGundong").jcarousellite_gundong({btn:1,list:".PictureList li","visible":2,"auto":5000,"speed":500,"afterEnd":function(){}})
+	$("#PhotoShowGundong2").jcarousellite_gundong({btn:1,list:".PictureList2 li","visible":1,"auto":4000,"speed":500,"afterEnd":function(){}})
+	$("#PhotoShowGundong3").jcarousellite_gundong({btn:1,list:".PictureList3 li","visible":2,"auto":4000,"speed":500,"afterEnd":function(){}})
+})
+
+
+
+
+//滚动
+		$.fn.jcarousellite_gundong=function(options ){
+			var self=$(this);
+			if (self.length==0) return false;
+			var items=options.list,config;
+			if (self.find(items).length<=options.visible) 
+			{
+				var width=self.find(options.list).parent().outerWidth()
+				self.css({"margin":"0 auto","width":width})
+			return false;	
+			}
+			else
+			{
+				var liobj=self.find(options.list)
+				var width=liobj.outerWidth()
+				var margin=parseInt(liobj.css("margin-left"))+parseInt(liobj.css("margin-right"));
+				width+=margin
+				self.css({"margin":"0 auto","width":width*options.visible})
+			}
+			self.css("overflow","visible");
+			
+			if (self.find(items).is("div"))
+			{
+				self.find(items).wrap("<li></li>");
+				self.find(items).parent().wrapAll("<ul class='templist'></ul>");		
+				items=".templist li"
+			}
+			self.find(items).parent().wrapAll('<div class="jCarouselLite"></div>').parent().wrapAll('<div class="gundong"></div>');
+			
+			
+			if (options.btn!=0)
+			{
+				self.find(".gundong").append('<span class="clear"></span><a href="javascript:;"  class="move_right"><span></span></a><a href="javascript:;" class="move_left disabled" ><span></span></a>')
+			}
+			
+			self.find(".gundong").each(function(index){
+				
+				config={
+							btnPrev: $(this).find(".move_left:eq("+index+")"),
+							btnNext: $(this).find(".move_right:eq("+index+")"),
+							visible:1,
+							auto: 2500 ,
+							speed: 300
+						}	
+				if (options.btn==0)		
+				{
+					$.extend(config, {btnPrev:null,btnNext:null});							
+				}
+				$.extend(config, options);
+				$(this).find(".jCarouselLite:eq("+index+")").jCarouselLite(config);	
+			})
+		}
+		
+	
+				
